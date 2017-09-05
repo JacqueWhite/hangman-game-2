66619ee585ac09937f827f677390a3a0b1218cdd
JacqueWhite <jacquecwhite@gmail.com>
2017-09-04 23:46:44 -0500

    adding on key up and start game functions

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,23 @@
+// *******************************************************
+// GLOBAL VARIABLES
+// *******************************************************
+var userGuess;
+var correctGuess;
+var wrongGuesses;
+var guessesLeft = 10;
+var wins = 0;
+var losses = 0;
+
+var chosenWord = "";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+
+//options...
+var easyList = ["roy", "jim", "pam"];
+var intermediateList = ["bears", "beets", "battlestar"];
+var hardList = ["yesh", "dundee", "scarn"];
+
 // ask the user's name only on the homepage...
 $( function() {
 	if ($('body').is('#welcome')) {
@@ -5,6 +25,83 @@ $( function() {
 		$("#userName").html(name);
 	}
 });
+
+// *******************************************************
+// EASY MODE
+// *******************************************************
+$( function() {
+	if ($('body').is('#easy')) {
+		function startGame() {
+
+		chosenWord = easyList[Math.floor(Math.random() * easyList.length)];
+
+		lettersInWord = chosenWord.split("");
+
+		numBlanks = lettersInWord.length;
+
+		console.log(chosenWord);
+
+		};
+		startGame();
+
+		$("#easy").keyup(function(event) {
+			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+			console.log(userGuess);
+		});
+	}
+});
+// *******************************************************
+// INTERMEDIATE MODE
+// *******************************************************
+$( function() {
+	if ($('body').is('#intermediate')) {
+		function startGame() {
+
+		chosenWord = intermediateList[Math.floor(Math.random() * intermediateList.length)];
+
+		lettersInWord = chosenWord.split("");
+
+		numBlanks = lettersInWord.length;
+
+		console.log(chosenWord);
+
+		};
+		startGame();
+
+		$("#intermediate").keyup(function(event) {
+				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+				console.log(userGuess);
+		});
+	}
+});
+// *******************************************************
+// HARD MODE
+// *******************************************************
+$( function() {
+	if ($('body').is('#hard')) {
+		function startGame() {
+
+		chosenWord = hardList[Math.floor(Math.random() * hardList.length)];
+
+		lettersInWord = chosenWord.split("");
+
+		numBlanks = lettersInWord.length;
+
+		console.log(chosenWord);
+
+		};
+
+		startGame();
+
+		$("#hard").keyup(function(event) {
+			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+			console.log(userGuess);
+		});
+	}
+});
+// *******************************************************
+// 
+// *******************************************************
 
 
 
